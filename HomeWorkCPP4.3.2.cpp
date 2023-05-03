0ffa602bd7d31e362b184ddddd6e694257eceff8
Andrey V <Andreyxvv@hotmail.com>
2023-05-03 22:48:00 +0500

    error correction

--- HomeWorkCPP4.3.2.cpp
+++ HomeWorkCPP4.3.2.cpp
@@ -1,36 +1,50 @@
 ﻿#include <iostream>
 #include <locale.h>
 
-class Smart_array {
+class SmartArray {
 public:
-
     // Копирование массива 
-    Smart_array& operator = (const Smart_array& other) {
-        delete[] array;
-        array = new int[other.size_];
-        int i = 0;
-        for (; i < other.size_; i++) {
-            array[i] = other.array[i];
+    SmartArray& operator = (const SmartArray& other) {
+        if (this != &other) {
+            size_ = other.size_;
+            num_array = other.num_array;
+
+            delete[] array;
+            array = new int[size_];
+
+            for (int i = 0; i < size_; i++) {
+                array[i] = other.array[i];
+            }
         }
-        num_array = i;
         return *this;
     }
 
-    Smart_array(int size) {
+    SmartArray (const SmartArray& other) {
+        if (this != &other) {
+            size_ = other.size_;
+            num_array = other.num_array;
+
+            array = new int[size_];
+
+            for (int i = 0; i < size_; i++) {
+                array[i] = other.array[i];
+            }
+        }
+    }
+
+    SmartArray(int size) {
         array = new int[size] {};
         size_ = size;
     }
-    ~Smart_array() {
+
+    ~SmartArray() {
         delete[] array;
         array = nullptr;
         delete[] array_new;
         array_new = nullptr;
     }
 
-    void add_element(int number) {
-        number_ = number;
-        element();
-    }
+    void add_element(int number);
 
     int get_element(int array_out) {
         return array[array_out];
@@ -41,14 +55,12 @@ private:
     int* array_new = nullptr;
     int size_ = 1;
     int num_array = 0;
-    int number_ = 0;
+   
     // Изменение размера массива
     void changing_array();
-    void element();
-
 };
 
-void Smart_array::changing_array() {
+void SmartArray::changing_array() {
 
     int size_new = 2 * size_;
     array_new = new int[size_new] {};
@@ -64,31 +76,32 @@ void Smart_array::changing_array() {
     array_new = nullptr;
 }
 
-void Smart_array::element() {
+void SmartArray::add_element(int number) {
 
     if (num_array == size_) {
         changing_array();
     }
 
-    array[num_array] = number_;
+    array[num_array] = number;
     num_array++;
 }
-
-
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    Smart_array arr(5);
+    SmartArray arr(5);
     arr.add_element(1);
     arr.add_element(4);
     arr.add_element(155);
 
-    Smart_array new_array(2);
+    SmartArray new_array(2);
     new_array.add_element(44);
     new_array.add_element(34);
+
+    SmartArray arr2(arr);
 
     arr = new_array;
 
     std::cout << arr.get_element(1) << std::endl;
+    std::cout << arr2.get_element(2) << std::endl;
 }
